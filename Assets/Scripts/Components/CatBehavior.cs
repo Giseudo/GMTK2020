2c17765d86c3c1b22b0793d8c5fc5b5135b1c243
Giseudo Oliveira <giseudo.oliveira@convenia.com.br>
2020-07-10 18:55:43 -0300

    Created CatData scriptable object

---
+++ Assets/Scripts/Components/CatBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatBehavior : MonoBehaviour {
+    public CatData data;
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
---
+++ Assets/Scripts/Scriptables/CatData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CatData", menuName = "Data/CatData", order = 1)]
+public class CatData : ScriptableObject {
+    public string name;
+    public bool thief;
+    public float eatTime;
+}
