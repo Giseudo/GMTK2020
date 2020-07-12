6e1dd01f48133730f43cb89e64502b7d34264932
Giseudo Oliveira <giseudo.oliveira@convenia.com.br>
2020-07-12 10:17:30 -0300

    Improved yarnball

--- Assets/Scripts/States/PlayingState.cs
+++ Assets/Scripts/States/PlayingState.cs
@@ -4,23 +4,43 @@ using UnityEngine;
 
 public class PlayingState : State {
     public Transform ball;
+    public float reachTime = 0f;
+    public float startTime = 0f;
     public PlayingState (Cat cat, StateMachine stateMachine) : base(cat, stateMachine) { }
 
     public override void Enter(State previousState) {
         base.Enter(previousState);
 
+        startTime = Time.unscaledTime;
         cat.agent.speed += 5f;
     }
 
     public override void Exit(State nextState) {
         base.Exit(nextState);
 
+        reachTime = 0f;
+        startTime = 0f;
         cat.agent.speed = cat.data.walkSpeed;
+        cat.agent.isStopped = false;
     }
 
     public override void LogicUpdate () {
         base.LogicUpdate();
 
         cat.agent.destination = ball.position;
+
+        if (cat.agent.remainingDistance <= .25f) {
+            cat.agent.isStopped = true;
+            reachTime = Time.unscaledTime;
+        }
+
+        if (reachTime + 2f < Time.unscaledTime) {
+            reachTime = 0f;
+            cat.agent.isStopped = false;
+        }
+
+        if (startTime + 10f < Time.unscaledTime) {
+            stateMachine.ChangeState(cat.idling);
+        }
     }
 }
